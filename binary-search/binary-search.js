a9df13dbcedc29f5194a860627436b6bfd7fb684
Matthew Chen <mc.2green@gmail.com>
2019-10-23 16:22:05 -0400

    completedbinarysearch

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,16 +2,22 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let firstIndex = 0;
+	let lastIndex = array.length - 1;
+	let middleIndex = Math.floor(array.length / 2);
+  console.log(middleIndex)
+
+	while(array[middleIndex] !== target && firstIndex < lastIndex ){
+    //console.log(middleIndex);
+		if(array[middleIndex] < target){
+			firstIndex = middleIndex + 1;
+		}
+    if(array[middleIndex] > target){
+			lastIndex = middleIndex - 1;
+		}
+		middleIndex = Math.floor((firstIndex + lastIndex) /2)
+	}
+	return (array[middleIndex] !== target) ? false : true
 };
-
-/*
-	EXTRA CREDIT:
-
-	Can you augment the function above to run in constant O(1) space?
-	This means, you cannot edit/copy the original array!
-	How can we use other pieces of data, like pointers, to avoid slicing?
-
-*/
 
 module.exports = binarySearch
